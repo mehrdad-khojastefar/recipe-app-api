21ee5986dfdaa1a4a3435fbd26a809ba01dd6b17
Mehrdad Khojastefar <mehrdadkh.uni@gmail.com>
2022-07-28 20:25:46 +0430

    added token authentication

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -9,6 +9,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse("user:create")
+TOKEN_URL = reverse("user:token")
 
 
 def create_user(**params):
@@ -60,3 +61,39 @@ class PublicUserApiTests(TestCase):
         # the transaction was not valid so no user object must be created in the database.
         user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test generates token for valid credentials."""
+        user_credentials = {
+            "name": "Test Name",
+            "email": "test@example.com",
+            "password": "test_user_password",
+        }
+        create_user(**user_credentials)
+
+        payload = {
+            "email": user_credentials["email"],
+            "password": user_credentials["password"],
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test returns error if credentials are invalid."""
+        create_user(email="test@example.com", password="goodpass")
+
+        payload = {"email": "test@example.com", "password": "badpass"}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn("member", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_blank_password(self):
+        """Test posting a blank password returns an error."""
+        payload = {"email": "test@example.com", "password": ""}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
